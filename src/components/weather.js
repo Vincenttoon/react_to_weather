171ef8233627aba6e952cc9acafd32d2cf8aadb4
Vincent Toon <vincenttoon22@gmail.com>
2023-01-25 19:17:31 -0600

    button giving me fits

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,19 +1,23 @@
 import React from "react";
 import "./styles.css";
 import moment from "moment";
-import { Button } from 'semantic-ui-react';
+import { Button } from "semantic-ui-react";
 
 const refresh = () => {
-    window.location.reload();
-}
+  window.location.reload();
+};
 
 const WeatherCard = ({ weatherData }) => (
   <div className="main">
-    <p className="header">{weatherData.name}</p>
+    <div className="top">
+      <p className="header">{weatherData.name}</p>
+      <Button className="button" color='grey' circular icon='refresh' onClick={refresh} />
+    </div>
     <div className="detail-container">
       <div className="flex">
         <p className="day">
-          {moment().format("dddd")}, <span className="date"> {moment().format("LL")}</span>
+          {moment().format("dddd")},{" "}
+          <span className="date"> {moment().format("LL")}</span>
         </p>
         <p className="description">{weatherData.weather[0].main}</p>
       </div>
