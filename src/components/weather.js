4c64f3a6e545f84b00948fd671c65176ceea696a
Vincent Toon <vincenttoon22@gmail.com>
2023-01-25 18:45:56 -0600

    card styled and retrieving data

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,22 +1,41 @@
-import React from 'react';
-import './styles.css';
-import { Card } from 'semantic-ui-react';
-import moment from 'moment';
+import React from "react";
+import "./styles.css";
+import moment from "moment";
+import { Button } from 'semantic-ui-react';
 
-const CardExampleCard = ({ weatherData }) => (
-    <Card>
-        <Card.Content>
-            <Card.Header className='header'>{weatherData.name}</Card.Header>
-            <p>Day: {moment().format('dddd')}</p>
-            <p>Date: {moment().format('LL')} </p>
-            <p>Temperature: {weatherData.main.temp}</p>
-            <p>Humidity: {weatherData.main.humidity}</p>
-            <p>Wind: {weatherData.wind.speed} mph</p>
-            <p>Sunrise: {weatherData.sys.sunrise}</p>
-            <p>Sunset: {weatherData.sys.sunset}</p>
-            <p>Description: {weatherData.weather[0].main}</p>
-        </Card.Content>
-    </Card>
-)
+const refresh = () => {
+    window.location.reload();
+}
 
-export default CardExampleCard;
+const WeatherCard = ({ weatherData }) => (
+  <div className="main">
+    <p className="header">{weatherData.name}</p>
+    <div className="detail-container">
+      <div className="flex">
+        <p className="day">
+          {moment().format("dddd")}, <span className="date"> {moment().format("LL")}</span>
+        </p>
+        <p className="description">{weatherData.weather[0].main}</p>
+      </div>
+
+      <div className="flex">
+        <p className="sunrise-sunset">
+          Sunrise{" "}
+          {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString("en-IN")}
+        </p>
+        <p className="sunrise-sunset">
+          Sunset{" "}
+          {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString("en-IN")}
+        </p>
+      </div>
+
+      <div className="flex">
+        <p className="temp">Temprature: {weatherData.main.temp} &deg;F</p>
+        <p className="temp">Humidity: {weatherData.main.humidity}%</p>
+        <p className="temp">Wind: {weatherData.wind.speed} mph</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default WeatherCard;
